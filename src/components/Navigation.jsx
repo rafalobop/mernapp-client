f4702cd258f5f91275445e83fc582ff5b37baf4a
unknown <r.loboplaza@hotmail.com>
2021-07-24 15:10:34 -0300

    agregando rutas

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 const Navigation = () => {
   return (
     <>
-      <h1>navigation</h1>
+      <Navbar collapseOnSelect expand="md" variant="dark" bg="dark">
+        <Navbar.Brand>
+          <Link to="/">TaskManager</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link>
+              <Link to="/proyects">Proyectos</Link>
+            </Nav.Link>
+            <NavDropdown title="Admin">
+              <NavDropdown.Item>
+                <Link to="/admin/users">Usuarios</Link>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link>
+              <Link to="/login">Iniciar Sesion</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/register">Registrarse</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/account">Mi Cuenta</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 };
